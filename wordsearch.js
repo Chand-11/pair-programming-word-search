d300157db8369518ea05748b48619a9432b1cf9e
chandni <chandnivaghasia11@gmail.com>
2021-07-30 22:56:46 -0400

    wordsearch.js

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,26 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    let string = [];
+    for (let i=0 ; i < letters[i].length; i++)
+    {
+        let temp;
+        for(let j=0 ; j < letters.length; j++)
+        {
+            temp += letters[j][i];
+        }
+        string.push(temp);
     }
+    for (const each of string) {
+        if(each.includes(word))
+        {
+            return true;
+        }
+    }
+    for (l of horizontalJoin) {
+        if (l.includes(word)) {
+        return true
+    }
+    }
+    return false;
 }
-
-module.exports = wordSearch
+module.exports = wordSearch;
